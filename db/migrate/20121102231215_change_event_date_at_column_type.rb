574acc65186496f9dbb4d32fbc80d16909cade86
jhlive <kingblak_katanga@hotmail.com>
2012-11-26 23:05:26 -0600

    fixing bugs for deployment

--- db/migrate/20121102231215_change_event_date_at_column_type.rb
+++ db/migrate/20121102231215_change_event_date_at_column_type.rb
@@ -1,9 +1,9 @@
 class ChangeEventDateAtColumnType < ActiveRecord::Migration
   def up
-  	change_column(:events, :start_at, :date)
+  	#change_column(:events, :start_at, :date)
   end
 
   def down
-  	change_column(:events, :start_at, :datetime)
+  	#change_column(:events, :start_at, :datetime)
   end
 end
