a1ad9dcb548485d3d4e41b48992d3c00ca433955
jhlive <kingblak_katanga@hotmail.com>
2012-11-27 00:02:17 -0600

    fixing bugs for deployment

--- app/controllers/mobile_app/events_controller.rb
+++ app/controllers/mobile_app/events_controller.rb
@@ -23,7 +23,7 @@ class MobileApp::EventsController < MobileApp::MobileController
   	respond_to do |format|
   		format.html
   		format.mobile
-  		format.json { json: {:events=>@events,:date_for_calendar=>@date_for_calendar,:date_at => @date_at}}
+  		format.json { json: { events: @events,date_for_calendar: @date_for_calendar, date_at: @date_at}}
   	end 
   end
 
