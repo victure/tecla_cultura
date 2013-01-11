59aa4a4df030d20aafa47255bea6a1887f6399b3
jhlive <kingblak_katanga@hotmail.com>
2013-01-11 00:20:03 -0600

    adding fog storage

--- app/uploaders/ckeditor_attachment_file_uploader.rb
+++ app/uploaders/ckeditor_attachment_file_uploader.rb
@@ -3,7 +3,7 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   include Ckeditor::Backend::CarrierWave
   
   # Include RMagick or ImageScience support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   # include CarrierWave::ImageScience
 
